adb22e059b4c352a63062875d0dd26d528fc6828
SakamakiDaiki <k4n1jp@yahoo.co.jp>
2021-12-24 00:14:35 +0900

    データの送受信

--- index.ts
+++ index.ts
@@ -3,5 +3,15 @@
 // {} = 要素の一つをインポートする (, で複数インポートも可)
 import  * as net from "net";
 import {connect} from "net";
+import {Buffer} from "buffer";
 
-console.log("Hello World!");
+const server = net.createServer(socket => {
+    socket.on("data", data => {
+        console.log(data.toString());
+
+        // データを受け取ったら送信
+        socket.write(Buffer.from("piyo"));
+    })
+});
+
+server.listen(3359);
